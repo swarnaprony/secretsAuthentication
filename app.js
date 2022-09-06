5d3b5a3d8c2dd796fdf7feebbe04c259ce04790b
Swarna Sarker <swarnaprony81@gmail.com>
2022-09-07 00:38:30 +0200

    Add passport authentication

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const GoogleStrategy = require( 'passport-google-oauth2').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -32,21 +33,57 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:4000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+  function(request, accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    console.log("Successful login");
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));  
+
 
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", {scope: ["profile"]})
+);
+
+app.get("/auth/google/secrets", function(req, res) {
+    passport.authenticate( 'google', {
+    successRedirect: '/secrets',
+    failureRedirect: '/login',
+    scope: ["email", "profile"]
+})
+}
+);
 
 app.get("/login", function(req, res){
     res.render("login");
